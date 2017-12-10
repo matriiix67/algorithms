0bd09dc156eeecfe15a5368b5a22cacb2c0ef2dd
matriiix67 <matriiix67@gmail.com>
2017-12-10 20:29:09 +0800

    delete c file

--- ohtbl.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Bourne on 2017/12/3.
-//
-
-#ifndef ALGORITHMS_OHTBL_H
-#define ALGORITHMS_OHTBL_H
-
-#endif //ALGORITHMS_OHTBL_H
--- set.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Bourne on 2017/12/3.
-//
-
-#ifndef ALGORITHMS_SET_H
-#define ALGORITHMS_SET_H
-
-#endif //ALGORITHMS_SET_H
